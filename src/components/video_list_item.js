2dfef23890e93fd316c80059db8d1b3e92c1208a
Varun Reddy <varunreddy1061@gmail.com>
2017-08-07 21:08:51 +0530

    added search_bar

---
+++ src/components/video_list_item.js
@@ -0,0 +1,5 @@
+//import React,{component} from 'react';
+//
+//const VideoListItem=(props)=>{
+//	return <li>Video</li>
+//};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+
+const API_KEY='AIzaSyDFuSqPK1rz986t7ykLi6syazyW6cCybmU';
+
+class App extends Component{
+	constructor(props){
+		super(props);
+
+		this.state={videos:''};
+
+		YTSearch({key:API_KEY,term:'surfboards'},(videos) => {
+			console.log(videos);
+			this.setState={videos};
+		});
+
+		render(){
+			<SearchBar/>
+			<VideoList videos={this.state.videos}/>
+		}
+	}
+}
+
+ReactDOM.render(<App/>,document.querySelector('.container'));
